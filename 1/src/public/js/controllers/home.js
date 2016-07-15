20323c366506490d657cfb096ebdba56578d4a79
lsanghoon <isanghoon@SHs-MacBook-Pro.local>
2016-07-15 13:29:10 +0900

    rm

--- 1/src/public/js/controllers/home.js
+++
@@ -1,5 +0,0 @@
-myApp.controller('homeCtrl', function ($location, $scope) {
-  $scope.scrollTo = function (hash) {
-    $location.hash(hash);
-  };
-});
--- Step01/app.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello World');
-console.log(process.arch);
-
-var url = require('url');
-
-var parsedObject = url.parse('http://www.naver.com/app?a=10&b=20');
-console.log(parsedObject);
-console.log(parsedObject.path);
-console.log(parsedObject.host);
